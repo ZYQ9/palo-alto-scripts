6dbae1e7e56d490fcc81ec9ab5975b77648f6336
Earl OConnor <earloconnor1@gmail.com>
2022-06-01 16:31:22 -0400

    remove sensitive filenames

--- address-cli/addr-cli.py
+++ address-cli/addr-cli.py
@@ -3,25 +3,25 @@
 txtfile = input('Enter the file you would like to send output to: ')
 
 with open(txtfile, 'w') as output:
-    with open('Fusion-Zoom.txt', 'r') as input:
+    with open('redacted', 'r') as input:
         output.write('======================= ZOOM ======================\n\n')
         for line in input:
             output.write('set address ' + 'ZN-' + line.replace('/','-').strip() + ' ' + 'ip-netmask ' + line)
         output.write('\n')
 
-    with open('Fusion-msteams.txt', 'r') as input:
+    with open('redacted', 'r') as input:
         output.write('======================= MS-TEAMS ======================\n\n')
         for line in input:
             output.write('set address ' + 'TN-' + line.replace('/','-').strip() + ' ' + 'ip-netmask ' + line)
         output.write('\n')
 
-    with open('Fusion-rc.txt', 'r') as input:
+    with open('redacted', 'r') as input:
         output.write('======================= RING CENTRAL ======================\n\n')
         for line in input:
             output.write('set address ' + 'RCN-' + line.replace('/','-').strip() + ' ' + 'ip-netmask ' + line)
         output.write('\n')
 
-    with open('Fusion-webex.txt', 'r') as input:
+    with open('redacted', 'r') as input:
         output.write('======================= CISCO WEBEX ======================\n\n')
         for line in input:
             output.write('set address ' + 'CWN-' + line.replace('/','-').strip() + ' ' + 'ip-netmask ' + line)
